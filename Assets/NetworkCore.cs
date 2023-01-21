a088d27d67006fcd51e32a28a2d63a7aaf8ae2d9
Furyn <vincent_1999@hotmail.fr>
2023-01-21 14:24:51 +0100

    [ADD] Client serialisation of STR + INT

--- Assets/NetworkCore.cs
+++ Assets/NetworkCore.cs
@@ -1,7 +1,8 @@
 using ENet;
 using System;
-using System.Collections;
-using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
 using UnityEngine;
 
 public class NetworkCore : MonoBehaviour
@@ -80,4 +81,90 @@ public class NetworkCore : MonoBehaviour
             while (m_enetHost.CheckEvents(out evt) > 0);
         }
     }
+
+    public static String DisplayBinary(Byte[] data)
+    {
+        return string.Join(" ", data.Select(byt => Convert.ToString(byt, 2).PadLeft(8, '0')));
+    }
+
+#region Int Serialisation
+    public static void Serialize_u32(ref byte[] byteArray, Int32 value)
+    {
+        int offset = byteArray.Length;
+        Array.Resize(ref byteArray, offset + sizeof(Int32));
+        Serialize_u32(ref byteArray, offset, value);
+    }
+
+    public static void Serialize_u32(ref byte[] byteArray, int offset, Int32 value)
+    {
+        //htonl;
+        value = IPAddress.HostToNetworkOrder(value);
+
+        byte[] valueByte = BitConverter.GetBytes(value);
+
+        for (int i = 0; i < valueByte.Length; i++)
+        {
+            byteArray[offset + i] = valueByte[i];
+        }
+    }
+
+    public static Int32 Unserialize_u32(ref byte[] byteArray, ref int offset)
+    {
+	    Int32 value;
+
+        byte[] intByte = new byte[sizeof(Int32)];
+
+        for (int i = offset; i < offset + sizeof(Int32); i++)
+        {
+            intByte[i - offset] = byteArray[i];
+        }
+
+        value = BitConverter.ToInt32(intByte);
+
+        //ntohl
+        value = IPAddress.NetworkToHostOrder(value);
+
+        offset += sizeof(Int32);
+
+	    return value;
+    }
+#endregion
+
+#region String Serialisation
+    public static void SerializeString(ref byte[] byteArray, string value)
+    {
+        int offset = byteArray.Length;
+        Array.Resize(ref byteArray, offset + sizeof(Int32) + value.Length);
+        SerializeString(ref byteArray, offset, value);
+    }
+
+    public static void SerializeString(ref byte[] byteArray, int offset, string value)
+    {
+        Serialize_u32(ref byteArray, offset ,value.Length);
+        offset += sizeof(int);
+
+        byte[] valueByte = System.Text.Encoding.ASCII.GetBytes(value);
+
+        for (int i = 0; i < value.Length; i++)
+        {
+            byteArray[offset + i] = valueByte[i];
+        }
+    }
+
+    public static string Unserialize_str(ref byte[] byteArray, ref int offset)
+    {
+	    int length = Unserialize_u32(ref byteArray, ref offset);
+
+        byte[] strByte = new byte[length];
+
+        for (int i = offset; i < offset+length; i++)
+        {
+            strByte[i - offset] = byteArray[i];
+        }
+
+        offset += length;
+        return Encoding.ASCII.GetString(strByte);
+    }
+#endregion
+
 }
